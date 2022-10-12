c3758883498a96bed2263d5abe2e9f2bd0b98065
Denisdif <ferreiradenisiv@gmail.com>
2022-10-12 19:03:46 -0300

    ABM de tipo de incidencia creado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,20 +36,23 @@
 					@auth()
                     <ul class="navbar-nav mr-auto">
 						<!--Nav Bar Hooks - Do not delete!!-->
+						<li class="nav-item">
+                            <a href="{{ url('/tipos_de_incidencias') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Tipos de incidencia </a> 
+                        </li>
 						{{-- <li class="nav-item">
-                            <a href="{{ url('/horas_extras') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Horas extras</a> 
+                            <a href="{{ url('/horas_extras') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Horas extras </a> 
                         </li> --}}
 						{{-- <li class="nav-item">
-                            <a href="{{ url('/incidencias') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Incidencias</a> 
+                            <a href="{{ url('/incidencias') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Incidencias </a> 
                         </li> --}}
 						{{-- <li class="nav-item">
-                            <a href="{{ url('/jornadas') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Jornadas</a> 
+                            <a href="{{ url('/jornadas') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Jornadas </a> 
                         </li> --}}
 						<li class="nav-item">
-                            <a href="{{ url('/empleados') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Empleados</a> 
+                            <a href="{{ url('/empleados') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Empleados </a> 
                         </li>
 						<li class="nav-item">
-                            <a href="{{ url('/categorias_de_horarios') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Categorias de horarios</a> 
+                            <a href="{{ url('/categorias_de_horarios') }}" class="nav-link"><i class="fab fa-laravel text-info"></i> Categorias de horarios </a> 
                         </li>
                     </ul>
 					@endauth()
